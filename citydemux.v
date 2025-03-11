ccd8568fff6776ae03c242f6b2bc4d025746b428
Harrison Jansen van Beek <harrisonjansenva@u.boisestate.edu>
2025-03-11 14:16:20 -0600

    updated variable names

--- citydemux.v
+++ citydemux.v
@@ -1,13 +1,13 @@
-module citydemux (enable, in, select, Y1, Y2, Y3, Y4);
+module citydemux (enable, in, select, library, fire, school, rib_shack);
 input enable;
 input [3:0] in;
 input [1:0] select;
-output [3:0] Y1, Y2, Y3, Y4;
+output [3:0] library, fire, school, rib_shack;
 
-assign Y1 = (enable && select == 0) ? in : 4'b0;
-assign Y2 = (enable && select == 1) ? in : 4'b0;
-assign Y3 = (enable && select == 2) ? in : 4'b0;
-assign Y4 = (enable && select == 3) ? in : 4'b0;
+assign library = (enable && select == 0) ? in : 4'b0;
+assign fire = (enable && select == 1) ? in : 4'b0;
+assign school = (enable && select == 2) ? in : 4'b0;
+assign rib_shack = (enable && select == 3) ? in : 4'b0;
 
 
     
