82ab0045537f6b6fd45ae398021c6f3cd1339bb3
Harrison Jansen van Beek <harrisonjansenva@u.boisestate.edu>
2025-03-11 14:12:14 -0600

    updated functionality

--- citydemux.v
+++ citydemux.v
@@ -1,12 +1,13 @@
-module citydemux (in, select, Y1, Y2, Y3, Y4);
+module citydemux (enable, in, select, Y1, Y2, Y3, Y4);
+input enable;
 input [3:0] in;
 input [1:0] select;
 output [3:0] Y1, Y2, Y3, Y4;
 
-assign Y1 = select == 0 ? in : 4'b0;
-assign Y2 = select == 1 ? in : 4'b0;
-assign Y3 = select == 2 ? in : 4'b0;
-assign Y4 = select == 3 ? in : 4'b0;
+assign Y1 = (enable && select == 0) ? in : 4'b0;
+assign Y2 = (enable && select == 1) ? in : 4'b0;
+assign Y3 = (enable && select == 2) ? in : 4'b0;
+assign Y4 = (enable && select == 3) ? in : 4'b0;
 
 
     
--- citymux.v
+++ citymux.v
@@ -1,13 +1,15 @@
-module citymux (ceo_data, my_data, fred_data, jill_data, select, Y);
+module citymux (enable, ceo_data, my_data, fred_data, jill_data, select, Y);
 
+    input enable;
     input [3:0] ceo_data, my_data, fred_data, jill_data;
     input [1:0] select;
     output [3:0] Y;
 
 
-    assign Y =  select == 0 ? ceo_data :
+    assign Y =  enalbe && (
+                select == 0 ? ceo_data :
                 select == 1 ? my_data :
-                select == 2 ? fred_data : jill_data;
+                select == 2 ? fred_data : jill_data) ? 4'b0;
 
     
 endmodule
